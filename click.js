1cd6d1b0d7f08c52207b00e3b7a71592170c2bde
jungin7612 <jungin7612@gmail.com>
2021-04-08 10:46:52 +0900

    텍스트 랜덤 추가

--- click.js
+++ click.js
@@ -32,24 +32,13 @@ let subtract = () => {
   }
 };
 /*
-item[0].addEventListener("click", function () {
-  if (a >= 5) {
-    text[0].innerText = "그만해";
-  } else {
-    
-    text[0].innerText = rearray[a];
-    a += 1;
-    console.log(a);
-  }
-});
-*/
+
 
 /* year change event */
 let dateValue = 0;
 let optionvalue = "";
 let sel = document.getElementById("years-select");
 let changeYears = () => {
-  
   for (dateValue = 0; dateValue < sel.options.length; dateValue++) {
     if (sel.options[dateValue].selected == true) {
       optionvalue = sel.options[dateValue].value;
@@ -61,7 +50,6 @@ let changeYears = () => {
   localStorage.setItem("year", optionvalue);
   var useroption = localStorage.getItem("year");
   optionvalue = useroption;
-  //
   switch (optionvalue) {
     case "hs1":
       dateValue = 2;
@@ -93,26 +81,6 @@ const initialize = () => {
 };
 initialize();
 
-/* clock event */
-
-let YYMMDD = document.getElementById("YYMMDD");
-let time = document.getElementById("time");
-
-// function clock() {
-//   let today = new Date();
-//   let year = today.getFullYear();
-//   let month = today.getMonth() + 1;
-//   let date = today.getDate();
-
-//   let hours = today.getHours();
-//   let minutes = today.getMinutes();
-//   let seconds = today.getSeconds();
-//   time.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-//     minutes < 10 ? `0${minutes}` : minutes
-//   }:${seconds < 10 ? `0${seconds}` : seconds}`;
-//   YYMMDD.innerText = year + "년 " + month + "월 " + date + "일";
-// }
-
 let yearArray = ["November 18, 2021", "November 17, 2022", "November 16, 2023"];
 /* d-day event */
 function getDdays() {
@@ -138,207 +106,47 @@ function init1() {
   getDdays();
   setInterval(getDdays, 500);
 }
-// function init2() {
-//   clock();
-//   setInterval(clock, 500);
-// }
+
 init1();
-// init2();
 
-// // cherry blossom on off
-// let cherrycontainer = document.getElementById("blossom_container");
-// let cherryicon = document.getElementsByClassName("icon");
+// random text
+function randomText() {
+  let floattext = document.getElementsByClassName("floatText");
+  let tagtext = [
+    "정신 차리자",
+    "의대 가야지",
+    "연세대가 우리를 기다린다",
+    "컴퓨터공학과 !",
+    "연애는 대학가서",
+    '"성공적인 대학생활"',
+    "서카포",
+    "디미고 화이팅",
+    "인생은 실전이다",
+    '"시발점"',
+    "노베의 기적",
+    "드리블 누가 막을 건데.",
+    "200일의 전사",
+    "주식 볼 시간에 공부해",
+  ];
+  let length = tagtext.length;
+  while (length) {
+    // 랜덤한 배열 index 추출
+    let index = Math.floor(length-- * Math.random());
 
-// let cherryop = () => {
-//   let k = 0.6;
-//   if (cherryicon[0].classList.contains("off")) {
-//     cherrycontainer.classList.remove("off");
-//   } else {
-//     cherrycontainer.classList.add("off");
-//   }
-// };
+    // 배열의 끝에서부터 0번째 아이템을 순차적으로 대입
+    let temp = tagtext[length];
 
-// let cherryblossomOnoff = () => {
-//   cherryop();
-//   if (!cherryicon[0].classList.contains("off")) {
-//     cherryicon[0].classList.add("off");
-//   } else {
-//     cherryicon[0].classList.remove("off");
-//   }
-// };
-//cherry blossom
-// var Petal = /** @class */ (function () {
-//   function Petal(config) {
-//     this.customClass = config.customClass || "";
-//     this.x = config.x || 0;
-//     this.y = config.y || 0;
-//     this.z = config.z || 0;
-//     this.xSpeedVariation = config.xSpeedVariation || 0;
-//     this.ySpeed = config.ySpeed || 0;
-//     this.rotation = {
-//       axis: "X",
-//       value: 0,
-//       speed: 0,
-//       x: 0,
-//     };
-//     if (config.rotation && typeof config.rotation === "object") {
-//       this.rotation.axis = config.rotation.axis || this.rotation.axis;
-//       this.rotation.value = config.rotation.value || this.rotation.value;
-//       this.rotation.speed = config.rotation.speed || this.rotation.speed;
-//       this.rotation.x = config.rotation.x || this.rotation.x;
-//     }
-//     this.el = document.createElement("div");
-//     this.el.className = "petal  " + this.customClass;
-//     this.el.style.position = "absolute";
-//     this.el.style.backfaceVisibility = "visible";
-//   }
-//   return Petal;
-// })();
-// var BlossomScene = /** @class */ (function () {
-//   function BlossomScene(config) {
-//     var container = document.getElementById(config.id);
-//     if (container === null) {
-//       throw new Error("[id] provided was not found in document");
-//     }
-//     this.container = container;
-//     this.placeholder = document.createElement("div");
-//     this.petals = [];
-//     this.numPetals = config.numPetals || 10;
-//     this.petalsTypes = config.petalsTypes;
-//     this.gravity = config.gravity || 0.8;
-//     this.windMaxSpeed = config.windMaxSpeed || 4;
-//     this.windMagnitude = 0.2;
-//     this.windDuration = 0;
-//     this.width = this.container.offsetWidth;
-//     this.height = this.container.offsetHeight;
-//     this.timer = 0;
-//     this.container.style.overflow = "hidden";
-//     this.placeholder.style.transformStyle = "preserve-3d";
-//     this.placeholder.style.width = this.container.offsetWidth + "px";
-//     this.placeholder.style.height = this.container.offsetHeight + "px";
-//     this.container.appendChild(this.placeholder);
-//     this.createPetals();
-//     requestAnimationFrame(this.updateFrame.bind(this));
-//   }
-//   /**
-//    * Reset the petal position when it goes out of container
-//    */
-//   BlossomScene.prototype.resetPetal = function (petal) {
-//     petal.x = this.width * 2 - Math.random() * this.width * 1.75;
-//     petal.y = petal.el.offsetHeight * -1;
-//     petal.z = Math.random() * 200;
-//     if (petal.x > this.width) {
-//       petal.x = this.width + petal.el.offsetWidth;
-//       petal.y = (Math.random() * this.height) / 2;
-//     }
-//     // Rotation
-//     petal.rotation.speed = Math.random() * 10;
-//     var randomAxis = Math.random();
-//     if (randomAxis > 0.5) {
-//       petal.rotation.axis = "X";
-//     } else if (randomAxis > 0.25) {
-//       petal.rotation.axis = "Y";
-//       petal.rotation.x = Math.random() * 180 + 90;
-//     } else {
-//       petal.rotation.axis = "Z";
-//       petal.rotation.x = Math.random() * 360 - 180;
-//       // looks weird if the rotation is too fast around this axis
-//       petal.rotation.speed = Math.random() * 3;
-//     }
-//     // random speed
-//     petal.xSpeedVariation = Math.random() * 0.8 - 0.4;
-//     petal.ySpeed = Math.random() + this.gravity;
-//     return petal;
-//   };
-//   /**
-//    * Calculate wind speed
-//    */
-//   BlossomScene.prototype.calculateWindSpeed = function (t, y) {
-//     var a =
-//       ((this.windMagnitude / 2) * (this.height - (2 * y) / 3)) / this.height;
-//     return (
-//       a *
-//         Math.sin(((2 * Math.PI) / this.windDuration) * t + (3 * Math.PI) / 2) +
-//       a
-//     );
-//   };
-//   /**
-//    * Update petal position
-//    */
-//   BlossomScene.prototype.updatePetal = function (petal) {
-//     var petalWindSpeed = this.calculateWindSpeed(this.timer, petal.y);
-//     var xSpeed = petalWindSpeed + petal.xSpeedVariation;
-//     petal.x -= xSpeed;
-//     petal.y += petal.ySpeed;
-//     petal.rotation.value += petal.rotation.speed;
-//     var t =
-//       "translateX( " +
-//       petal.x +
-//       "px ) translateY( " +
-//       petal.y +
-//       "px ) translateZ( " +
-//       petal.z +
-//       "px )  rotate" +
-//       petal.rotation.axis +
-//       "( " +
-//       petal.rotation.value +
-//       "deg )";
-//     if (petal.rotation.axis !== "X") {
-//       t += " rotateX(" + petal.rotation.x + "deg)";
-//     }
-//     petal.el.style.transform = t;
-//     // reset if out of view
-//     if (petal.x < -10 || petal.y > this.height + 10) {
-//       this.resetPetal(petal);
-//     }
-//   };
-//   /**
-//    * Change the wind speed
-//    */
-//   BlossomScene.prototype.updateWind = function () {
-//     // wind duration should be related to wind magnitude, e.g. higher windspeed means longer gust duration
-//     this.windMagnitude = Math.random() * this.windMaxSpeed;
-//     this.windDuration = this.windMagnitude * 50 + (Math.random() * 20 - 10);
-//   };
-//   /**
-//    * Create the petals elements
-//    */
-//   BlossomScene.prototype.createPetals = function () {
-//     for (var i = 0; i < this.numPetals; i++) {
-//       var tmpPetalType = this.petalsTypes[
-//         Math.floor(Math.random() * (this.petalsTypes.length - 1))
-//       ];
-//       var tmpPetal = new Petal({ customClass: tmpPetalType.customClass });
-//       this.resetPetal(tmpPetal);
-//       this.petals.push(tmpPetal);
-//       this.placeholder.appendChild(tmpPetal.el);
-//     }
-//   };
-//   /**
-//    * Update the animation frame
-//    */
-//   BlossomScene.prototype.updateFrame = function () {
-//     if (this.timer === this.windDuration) {
-//       this.updateWind();
-//       this.timer = 0;
-//     }
-//     var petalsLen = this.petals.length;
-//     for (var i = 0; i < petalsLen; i++) {
-//       this.updatePetal(this.petals[i]);
-//     }
-//     this.timer++;
-//     requestAnimationFrame(this.updateFrame.bind(this));
-//   };
-//   return BlossomScene;
-// })();
-// var petalsTypes = [
-//   new Petal({ customClass: "petal-style1" }),
-//   new Petal({ customClass: "petal-style2" }),
-//   new Petal({ customClass: "petal-style3" }),
-//   new Petal({ customClass: "petal-style4" }),
-// ];
-// var myBlossomSceneConfig = {
-//   id: "blossom_container",
-//   petalsTypes: petalsTypes,
-// };
-// var myBlossomScene = new BlossomScene(myBlossomSceneConfig);
+    // 랜덤한 위치의 값을 맨뒤(this[length])부터 셋팅
+    tagtext[length] = tagtext[index];
+
+    // 랜덤한 위치에 위에 설정한 temp값 셋팅
+    tagtext[index] = temp;
+  }
+
+  console.log(tagtext);
+
+  for (let i = 0; i < floattext.length; i++) {
+    floattext[i].innerText = tagtext[i];
+  }
+}
+randomText();
